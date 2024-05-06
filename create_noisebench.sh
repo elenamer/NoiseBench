c0af01e4606ae5e1da0b89929ac4325958722ac0
elenamer <elenamerdzanovska@yahoo.com>
2024-05-06 15:11:58 +0200

    add a self-contained script to create datasets

---
+++ create_noisebench.sh
@@ -0,0 +1,13 @@
+echo "Cloning CleanCoNLL repository to get the full CoNLL-03 dataset with CleanCoNLL annotations" 
+git clone https://github.com/flairNLP/CleanCoNLL.git
+cd CleanCoNLL
+chmod u+x create_cleanconll_from_conll03.sh
+SCRIPT_ROOT=. bash create_cleanconll_from_conll03.sh
+mkdir -p ../data/cleanconll
+mv ./data/cleanconll/* ../data/cleanconll
+cd ..
+rm -rf CleanCoNLL
+
+echo "Creating NoiseBench dataset files and train/dev splits" 
+python scripts/generate_data_files.py
+echo "Done!" 
